4eb4f22e4b54a7584e1325679a562d2cceee0a34
Arnoques <arnoques+git@gmail.com>
2013-11-03 02:25:59 -0300

    Original files from code.google.com/p/chm2pdf

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "chm2pdf",
+    version = "0.9",
+    description = "A script to convert CHM files into PDF",
+    author = "Massimo Sandal, Chris Karakas",
+    author_email = "devicerandom@gmail.com, chris@karakas-online.de",
+    url = "http://code.google.com/p/chm2pdf/",
+    scripts = ["chm2pdf"],
+    long_description = "A script to convert CHM files into PDF. Requires chmlib, pychm, htmldoc."
+    )
+    
+
+    
